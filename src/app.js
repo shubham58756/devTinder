cc106352f8af9d6e1ea7cb7a5695ac09fac57a1e
Shubham Patel <shubhampatel@example.com>
2025-12-23 14:41:36 +0530

    writing-auth-middleware

--- src/app.js
+++ src/app.js
@@ -1,14 +1,30 @@
 const express = require("express");
 const app = express();
 
-app.use("/user", (req, res) => {
+const rh1 = (req, res, next) => {
   console.log("route handler 1 called");
-  res.send("route handler 1");
-},
-()=>{
-  // route handler 2
+  req.user = "Rahul";
+  next();
+};
+
+const rh2 = (req, res, next) => {
   console.log("route handler 2 called");
-  res.send("route handler 2");
+  if (!req.user) {
+    next(new Error("User not found"));
+  } else {
+    next();
+  }
+};
+
+const rh3 = (req, res) => {
+  console.log("route handler 3 called");
+  res.send("Hello " + req.user);
+};
+
+app.use("/user", [rh1, rh2, rh3]);
+
+app.use((err, req, res, next) => {
+  res.status(500).send(err.message);
 });
 
 app.listen(7777, () => {
