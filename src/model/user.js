5515f862fbbccf0c1f3c2b425e28a04aa1cc5660
Shubham Patel <shubhampatel@example.com>
2025-12-24 22:25:22 +0530

    feat(user): unique email, timestamps, and hash passwords with bcrypt

--- src/model/user.js
+++ src/model/user.js
@@ -1,23 +1,41 @@
 const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs")
 
 const userSchema = new mongoose.Schema({
   firstName: {
-    type: String
+    type: String,
+    required: true,
   },
   lastName: {
-    type: String
+    type: String,
   },
   email: {
-    type: String
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
   },
   password: {
-    type: String
+    type: String,
+    required: true,
   },
   age: {
-    type: Number
+    type: Number,
   },
   gender: {
-    type: String
+    type: String,
+  },
+}, { timestamps: true })
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next()
+  try {
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+    return next()
+  } catch (err) {
+    return next(err)
   }
 })
 
