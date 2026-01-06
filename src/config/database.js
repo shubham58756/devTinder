6cf33d8938b9a6e8a3b6f7c0d5082b119900d583
Shubham Patel <shubhampatel@example.com>
2026-01-06 15:48:20 +0530

    db

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,19 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  console.log(process.env.DB_CONNECTION_SECRET);
-  await mongoose.connect(
-    'mongodb+srv://heyshubhampatel2005_db_user:coSvuwmBMssa2Hcc@cluster0.od1rngk.mongodb.net/devTinderDB?retryWrites=true&w=majority'
+  try {
+    console.log("Connecting to DB...");
+    await mongoose.connect(
+      'mongodb+srv://heyshubhampatel2005_db_user:coSvuwmBMssa2Hcc@cluster0.od1rngk.mongodb.net/devTinderDB?retryWrites=true&w=majority'
   );
+    
+    console.log("Database connection established.");
+  } catch (err) {
+    console.error("Database cannot be connected!!", err);
+    process.exit(1); // Optional: stop the server if DB fails
+  }
 };
 
 module.exports = connectDB;
+
+
