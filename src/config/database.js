8e52587907dd53db9901f4a1b6ba398949e64701
Shubham Patel <shubhampatel@example.com>
2026-01-06 14:52:31 +0530

    Database

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   console.log(process.env.DB_CONNECTION_SECRET);
   await mongoose.connect(
-    'mongodb+srv://heyshubhampatel2005_db_user:coSvuwmBMssa2Hcc.od1rngk.mongodb.net/devTinderDB?retryWrites=true&w=majority'
+    'mongodb+srv://heyshubhampatel2005_db_user:coSvuwmBMssa2Hcc@cluster0.od1rngk.mongodb.net/devTinderDB?retryWrites=true&w=majority'
   );
 };
 
